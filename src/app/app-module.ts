d05ebb709d1016d5b9d1a858cf86787a7a584a2e
Deepali Kadway <dkadway@gmail.com>
2025-08-13 16:36:14 -0600

    creating components

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Custom } from './components/custom/custom';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Custom
   ],
   imports: [
     BrowserModule,
