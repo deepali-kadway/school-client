d8a3dc3b5d68cd859c2804f25355952a114a1ff3
Deepali Kadway <dkadway@gmail.com>
2025-08-13 20:02:13 -0600

    class binding & ngClass directive

--- src/app/components/course/course.ts
+++ src/app/components/course/course.ts
@@ -2,19 +2,20 @@ import { Component } from '@angular/core';
 import { Courses } from '../../services/courses';
 
 @Component({
-  selector: 'app-course',
+  selector: 'course',
   standalone: false,
   templateUrl: './course.html',
-  styleUrl: './course.css'
+  styleUrl: './course.css',
 })
 export class Course {
   title: string = 'List of Courses';
-  courses: string[];
+  courses: string[] = [];
+  booleanVariable = false;
 
-  // dependency injection of Courses service
-  // when this component is created, angular finds and injects an instance of the courseService class into serviceVariable within constructor function
-  constructor(serviceVariable: Courses){
-    // we can use the serviceVariable to call getCourses function from courses service
+  // Dependency Injection
+  // When this component is created, angular finds and injects an instance of the Courses Service class and stores it into serviceVariable within constructor function.
+  constructor(serviceVariable: Courses) {
+    // We can use the serviceVariable to call getCourses function from Courses Service
     this.courses = serviceVariable.getCourses();
   }
 }
