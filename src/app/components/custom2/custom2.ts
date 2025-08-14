bfb59e849d2c507e298320674fe812961205b61f
Deepali Kadway <dkadway@gmail.com>
2025-08-14 16:41:57 -0600

    style binding & ngStyle directive

--- src/app/components/custom2/custom2.ts
+++ src/app/components/custom2/custom2.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class Custom2 {
   date = new Date();
+   boxColor = 'blue';
+  boxSize = 150;
+
+  changeStyle() {
+    // change color randomly
+    const colors: string[] = ['blue', 'green', 'red', 'purple', 'orange'];
+    // random function returns a number between 0->1
+    // floor function rounds down to nearest whole number: 3.632 -> 3
+    this.boxColor = colors[Math.floor(Math.random() * colors.length)];
+
+    // change the size randomly
+    this.boxSize = Math.floor(Math.random() * 200) + 100;
+  }
 }
